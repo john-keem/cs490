d729b80ef53ed3c360dca461aa6e3d0bd3dc8c0a
John <rladygks02@gmail.com>
2025-03-20 19:51:01 -0700

    Setup database to be used

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,23 @@ app.use(morgan("dev")); // log the requests
 
 app.use("/api/products", productRoutes);
 
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `
+  }
+  catch(error){
+    console.log("Error initDB", error);
+  }
+}
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
