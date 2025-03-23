3d0f3497d47cad1d5fac6eb685c27ff2ff1d883b
John <rladygks02@gmail.com>
2025-03-22 19:12:46 -0700

    Create the frontend layout/themes using React, tailwind, daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,14 @@ import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
 import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const {theme} = useThemeStore();
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
       
       <Navbar />
 
@@ -15,6 +19,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 }
