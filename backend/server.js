346fcc1c0f4235f0b10073e05ee86e72ff22e216
John <rladygks02@gmail.com>
2025-03-23 00:25:15 -0700

    Configure final changes for deploying app

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
@@ -12,10 +13,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(
+  helmet({
+    contentSecurityPolicy:false,
+  })
+); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
 app.use(morgan("dev")); // log the requests
 
 // apply arcjet rate-limit to all routes
@@ -52,6 +58,17 @@ app.use(async (req, res, next) => {
 });
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV==="production") {
+  // server our React app
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
+
+
 
 async function initDB() {
   try {
