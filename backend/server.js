367f15c194c80469ee7e93d4555c7969b38252a0
John <rladygks02@gmail.com>
2025-03-22 23:22:18 -0700

    Create fetch and delete products functionality, display no products page

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
 import { aj } from "./lib/arcjet.js";
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -17,19 +19,19 @@ app.use(helmet()); // helmet is a security middleware that helps you protect you
 app.use(morgan("dev")); // log the requests
 
 // apply arcjet rate-limit to all routes
-app.use(async(req,resizeBy,next) => {
+app.use(async (req, res, next) => {
   try {
     const decision = await aj.protect(req, {
-      requested:1 // specifies that each request consumes 1 token
-    })
+      requested: 1, // specifies that each request consumes 1 token
+    });
 
-    if(decision.isDenied()) {
-      if(decision.reason.isRateLimit()) {
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
         res.status(429).json({ error: "Too Many Requests" });
-      }
-      else if(decision.reason.isBot()) {
+      } 
+      else if (decision.reason.isBot()) {
         res.status(403).json({ error: "Bot access denied" });
-      }
+      } 
       else {
         res.status(403).json({ error: "Forbidden" });
       }
@@ -37,18 +39,17 @@ app.use(async(req,resizeBy,next) => {
     }
 
     // check for spoofed bots
-    if(decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
-      res.status(403).json({ error: "Spoofed bot detected "});
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
       return;
     }
 
     next();
-  }
-  catch(error) {
+  } catch (error) {
     console.log("Arcjet error", error);
     next(error);
   }
-})
+});
 
 app.use("/api/products", productRoutes);
 
